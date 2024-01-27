69dabbdb145318ec563a20d49eca56ccdd58c80a
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2024-01-27 14:43:21 +0500

    Done

---
+++ scripts.js
@@ -0,0 +1,112 @@
+const chatbotToggler = document.querySelector("#chatbot-img");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input #send-btn");
+const messageText = document.querySelector(".chat-input p");
+
+let arr = [];
+let count = 0;
+let key = 1;
+let userMessage = null; // Variable to store user's message
+
+const inputInitHeight = chatInput.scrollHeight;
+
+const createChatLi = (message, className) => {
+  // Create a chat <li> element with passed message and className
+
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : `<img src="robot.png" alt="Robot Image" width="45" height="50"><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+};
+
+const generateResponse = (chatElement) => {
+  // main function
+  const API_URL = "http://localhost:8000/getResponse";
+  const messageElement = chatElement.querySelector("p");
+
+  // Define the properties and message for the API request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      key: key,
+      user: userMessage,
+    }),
+  };
+
+  // Send POST request to API, get response and set the reponse as paragraph text
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      let { response, end } = { ...data };
+      console.log(response, end);
+      if (end == true) {
+        chatInput.style.display = "none";
+        sendChatBtn.style.display = "none";
+        messageText.style.display = "block";
+      }
+      key = 0;
+      messageElement.textContent = response;
+    })
+    .catch(() => {
+      messageElement.classList.add("error");
+      messageElement.textContent =
+        "Oops! Something went wrong. Please try again.";
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+  if (!userMessage) return;
+
+  // Clear the input textarea and set its height to default
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Append the user's message to the chatbox
+  chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
+    const incomingChatLi = createChatLi("Typing...", "incoming");
+    chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+  }, 600);
+};
+
+chatInput.addEventListener("input", () => {
+  // Adjust the height of the input textarea based on its content
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+  // If Enter key is pressed without Shift key and the window
+  // width is greater than 800px, handle the chat
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat();
+  }
+});
+
+// clearChatBtn.addEventListener("click", clearAllChats);
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+chatbotToggler.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
